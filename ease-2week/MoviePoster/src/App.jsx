2a941457cc3a7d40465b4f5e253aa9a8cef645b8
sihyun2988 <sihyun2988@gmail.com>
2024-05-01 16:56:46 +0900

    1, 2week

---
+++ ease-2week/MoviePoster/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Poster from './components/Poster.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <Poster />
+    </>
+  )
+}
+
+export default App
