8c30c3a746719f4bb0c86b10563d28f76c851823
solmee <kii5541@naver.com>
2024-05-11 18:51:08 +0900

    기초 세팅

---
+++ 5week/movie/src/components/banner/banner.jsx
@@ -0,0 +1,5 @@
+import { StyledBanner } from "./banner.style";
+
+export const Banner = () => {
+  return <StyledBanner>환영합니다</StyledBanner>;
+};
---
+++ 5week/movie/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+import { Banner } from "../../components/banner/banner";
+
+export const MainPage = () => {
+  return (
+    <>
+      <Banner />
+      <div>검색</div>
+    </>
+  );
+};
