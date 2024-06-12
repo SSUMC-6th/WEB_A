712129134ddc430641415aa3938bdc4c0e2b42d7
sihyun2988 <sihyun2988@gmail.com>
2024-06-13 01:26:29 +0900

    overview 추가

--- ease-8week/TMDBMovie/src/components/Movie.jsx
+++ ease-8week/TMDBMovie/src/components/Movie.jsx
@@ -14,9 +14,40 @@ const Vote = styled.div`
     color: white;
 `;
 
+const OverviewContainer  = styled.div`
+    background-color: rgba(0, 0, 0, 0.521);
+    position:absolute;
+    top: 0;
+    display: none;
+    flex-direction: column;
+    padding: 10% 6%;
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    border-radius: 2%;
+    gap: 6%;
+`;
+
+const OverviewSt  = styled.div`
+    color: white;
+    height: 9.5em;
+    overflow: hidden;
+    line-height: 1.2;
+    text-align: left;
+    word-wrap: break-word;
+    display:-webkit-box;
+    -webkit-line-clamp: 8;
+    -webkit-box-orient: vertical;
+`;
+
 const MovieContainer = styled.div`
+    position:relative;
     cursor: pointer;
     width: 166.66px;
+    &:hover ${OverviewContainer}{
+        display: flex;
+        cursor: pointer;
+    }
 `;
 
 const MovieImage  = styled.img`
@@ -27,15 +58,17 @@ const MovieImage  = styled.img`
 `;
 
 const InfoContainer  = styled.div`
-    display:flex;
+    display: flex;
     justify-content: space-between;
     padding: 4% 6% 30%;
     height: 30px;
     gap: 3%;
     background-color: rgba(240, 248, 255, 0.167);
-    border-bottom-left-radius: 5%;
-    border-bottom-right-radius: 5%;
+    border-bottom-left-radius: 4%;
+    border-bottom-right-radius: 4%;
 `;
+
+
 
 
 
@@ -47,11 +80,17 @@ function Movie({id, title, poster_path, vote_avg, release_date, overview}){
             <Poster><MovieImage
               alt="movieImage" 
               src= {`https://image.tmdb.org/t/p/w500/${poster_path}`}
-            /></Poster>
+            />
+            <OverviewContainer>
+                <Title>{title}</Title>
+                <OverviewSt>{overview}</OverviewSt>
+            </OverviewContainer>
+            </Poster>
             <InfoContainer>
                 <Title>{title}</Title>
                 <Vote>{vote_avg}</Vote>
             </InfoContainer>
+            
         </MovieContainer>
     )
 }
